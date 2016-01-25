34e3659e647285b5f5395cfaf77ca9fd83e3803d
Ingo Rößner <ingo.spicker@gmail.com>
2016-01-25 13:53:57 +0100

    add a legend for the map

--- visualization_map/ol3_main.js
+++ visualization_map/ol3_main.js
@@ -5,6 +5,7 @@ Dependencies: ol3_script.js, ol3_dialog.js, jquery
 var test;
 
 $(function(){
+    createLegende($("#legende"), 100, 25)
     showDialog(function(content){
         run(JSON.parse(content));
     });
