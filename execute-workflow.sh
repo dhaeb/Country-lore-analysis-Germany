299a9c62cce124953bfee4732bf4a30201fc4bcf
dhaeb <dan.haeberlein@googlemail.com>
2016-03-01 19:23:17 +0100

    changes for the path of the geojson files

--- execute-workflow.sh
+++ execute-workflow.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 R --no-save <<'EOF'
-source("analyse_scripts/init-sparkr.R")
-source("analyse_scripts/schemata.R")
-source("analyse_scripts/claf.R")
-analyseCountryLore(clInputJanHellSommerHeiss)
-analyseCountryLore(clInputNovFrostJanNass)
-analyseCountryLore(clInputOktoberWarmFein)
-analyseCountryLore(clInputOktoberNassKühl)
-analyseCountryLore(clInputMartiniWinter)
-analyseCountryLore(clInputFebruarNassJahrNass)
+  source("analyse_scripts/init-sparkr.R")
+  source("analyse_scripts/schemata.R")
+  source("analyse_scripts/claf.R")
+  analyseCountryLore(clInputJanHellSommerHeiss)
+  analyseCountryLore(clInputNovFrostJanNass)
+  analyseCountryLore(clInputOktoberWarmFein)
+  analyseCountryLore(clInputOktoberNassKühl)
+  analyseCountryLore(clInputMartiniWinter)
+  analyseCountryLore(clInputFebruarNassJahrNass)
 EOF
 export RESULT_FOLDER="./intermeditate_results"
 for f in $(find $RESULT_FOLDER -name "*.csv"); do
-  export TARGET="${f%.csv}_geo.json"
+  export FBASE=$(basename $f)
+  export TARGET="$RESULT_FOLDER/${FBASE%.csv}_geo.json"
   echo $TARGET
   groovy non_cluster_processes/csv-to-geojson.groovy $f > $TARGET;
 done
